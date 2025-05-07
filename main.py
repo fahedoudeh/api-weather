4765ac54f47b48aa7e53a2eae4b52dfc5ee0fc17
Fahed Oudeh <fahed.oudeh@gmail.com>
2025-05-07 16:54:30 +0200

    api weather is solved

--- main.py
+++ main.py
@@ -3,40 +3,27 @@ import requests
 
 
 def get_weather_data(city_name, api_key):
-    # API-endpoint
-    url = (f"http://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}&units=metric")
-
-    #Http GET request om data op te halen
+    url = (f'https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}&units=metric')
     response = requests.get(url)
+    return response
 
-    if response.status_code == 200:
-        return response.json()
-    else:
-        print(f"Error: {response.status_code} - {response.text}")
+def print_weather_data(data_dict):
+    print(f"City: {data_dict['name']}")
+    print(f"Country: {data_dict['sys']['country']}")
+    print(f"Weather: {data_dict['weather'][0]['main']}")
+    print(f"Description: {data_dict['weather'][0]['description']}")
+    print(f"Temp: {data_dict['main']['temp']} Degrees")
 
-def display_weather_info(weather_data):
-    city = weather_data["name"]
-    temp = weather_data["main"]["temp"]
-    description = weather_data["weather"][0]["description"]
-    humidity = weather_data["main"]["humidity"]
-    wind_speed = weather_data["wind"]["speed"]
-
-    print(f"Weather in {city}:")
-    print(f"Temperature: {temp}°C")
-    print(f"Description: {description.capitalize()}")
-    print(f"Humidity: {humidity}%")
-    print(f"Wind Speed: {wind_speed} m/s")
-
-
-
-
-api_key = "d6e205d1e4a404e57500568b85768c29"
-
-city_name = input("Enter city name: ").strip()
-
-while not city_name:
-    print("City name cannot be empty")
-    city_name= input("Enter city name").strip()
+city_name = input("Enter a city name: ")
+api_key = 'd6e205d1e4a404e57500568b85768c29'
 
 weather_data = get_weather_data(city_name, api_key)
-display_weather_info(weather_data)
+
+weather_data_dict = weather_data.json()
+
+if weather_data.status_code == 200:
+    print("Success!")
+else:
+    print(f"Something went wrong: {weather_data.status_code}")
+
+print_weather_data(weather_data_dict)
